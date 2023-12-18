6dcbbdf4a4d285e9fc06749e7d738c723a248ebb
V-Markovskis <vlads.markovskis@gmail.com>
2023-12-18 15:55:49 +0200

    css fixes

--- src/Pages/AboutAuthorPage/AboutAuthor.tsx
+++ src/Pages/AboutAuthorPage/AboutAuthor.tsx
@@ -3,12 +3,18 @@ const AboutAuthor = () => {
   return (
     <div className={style.aboutAuthorContainer}>
       <div className="col-4">
-        <h3>Hello! My name is Lorem.</h3>
-        <h6>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. A accusamus alias aliquid aperiam aut beatae
-          consequuntur dolor dolorum ducimus ea eaque eos fugit harum hic impedit inventore ipsa iste iusto libero
-          maiores maxime molestiae mollitia necessitatibus nihil odit optio pariatur perspiciatis quas, qui quibusdam
-          quos reprehenderit similique soluta ullam unde!
+        <div className={style.header}>
+          <a href="https://git.io/typing-svg">
+            <img
+              src="https://readme-typing-svg.herokuapp.com?font=Fira+Code&weight=600&pause=1000&center=true&vCenter=true&repeat=false&random=false&width=435&lines=Hello!+My+name+is+Vlads!"
+              alt="Typing SVG"
+            />
+          </a>
+        </div>
+        <h6 className={style.smallHeader}>
+          At the moment, I am learning the TypeScript programming language and experimenting with React and its features
+          in this project. I can't say that everything is going perfectly, but I'm trying. Thank you for dedicating time
+          to this project, and all the best to you.
         </h6>
       </div>
     </div>
